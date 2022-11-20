35fea6eb9e4012343d70001f4a645ff1fb6ad286
E-Code30 <edwardjefta55@gmail.com>
2022-11-21 06:56:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mylistview_f55121005/edward.java
@@ -0,0 +1,14 @@
+package com.example.mylistview_f55121005;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edward extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edward);
+    }
+}
